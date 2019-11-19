840c96dc9be6945bbf6d53abe0e2b3edc7358183
Gabriel Audette <gabaudette@gmail.com>
2019-11-19 15:28:49 -0500

    Create CargoPlane.cs

---
+++ TP-Builder/WindowsFormsApp1/CargoPlane.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPBuilder
+{
+    public class CargoPlane : PassengerAircraft
+    {
+        public CargoPlane(string name) : base(name)
+        {
+        }
+    }
+}
