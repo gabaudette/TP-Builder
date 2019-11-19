5ddc5733f3202d1f865cc50846a3f8042bb44e64
Gabriel Audette <gabaudette@gmail.com>
2019-11-19 15:08:05 -0500

    Create Aircraft.cs

---
+++ TP-Builder/WindowsFormsApp1/Aircraft.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPBuilder
+{
+    abstract class Aircraft{
+        public string AircraftName { get; set; }
+    }
+}
