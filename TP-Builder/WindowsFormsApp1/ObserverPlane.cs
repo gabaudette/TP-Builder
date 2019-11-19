758ded458647a675594de8037dd06a77ed7d003e
Gabriel Audette <gabaudette@gmail.com>
2019-11-19 15:28:50 -0500

    Create ObserverPlane.cs

---
+++ TP-Builder/WindowsFormsApp1/ObserverPlane.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPBuilder
+{
+    public class ObserverPlane : Aircraft
+    {
+        public ObserverPlane(string name) : base(name)
+        {
+        }
+    }
+}
