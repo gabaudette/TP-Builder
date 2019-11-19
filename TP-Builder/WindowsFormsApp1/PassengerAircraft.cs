a21ab3826a060cb397f0cbf9c13fa546a30e2250
Gabriel Audette <gabaudette@gmail.com>
2019-11-19 15:28:51 -0500

    Create PassengerAircraft.cs

---
+++ TP-Builder/WindowsFormsApp1/PassengerAircraft.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPBuilder
+{
+    public class PassengerAircraft : Aircraft
+    {
+        public PassengerAircraft(string name) : base(name)
+        {
+        }
+    }
+}
