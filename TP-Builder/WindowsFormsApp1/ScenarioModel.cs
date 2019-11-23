f91f0579bc39e1726f539378cfa94bb70c12637e
Gabriel Audette <gabaudette@gmail.com>
2019-11-23 18:13:07 -0500

    -Added Scenario Model -Add Aiport Model

---
+++ TP-Builder/WindowsFormsApp1/ScenarioModel.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace TPBuilder
+{
+    public sealed class ScenarioModel
+    {
+        private static ScenarioModel instance = null;
+        public List<Aircraft> Aircrafts { get; private set; }
+        public List<Airport> Airports { get; private set; }
+
+        private ScenarioModel(){}
+
+        public static ScenarioModel Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ScenarioModel();
+                }
+                return instance;
+            }
+        }
+
+        public void CreateAirport()
+        {
+
+        }
+
+        public void UpdateAirport()
+        {
+
+        }
+
+        public void DeleteAirport()
+        {
+
+        }
+
+        public void CreateAircraft()
+        {
+
+        }
+
+        public void UpdateAircraft()
+        {
+   
+        }
+
+        public void DeleteAircraft()
+        {
+
+        }
+    }
+}
