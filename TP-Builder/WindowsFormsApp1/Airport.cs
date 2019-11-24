f1a8da6b685335711221919b8de24419ab94b2dc
Gabriel Audette <gabaudette@gmail.com>
2019-11-23 19:30:29 -0500

    Updated Aiport

--- TP-Builder/WindowsFormsApp1/Airport.cs
+++ TP-Builder/WindowsFormsApp1/Airport.cs
@@ -3,14 +3,19 @@
     public class Airport
     {
         public string Name { get; private set; }
+        public int X { get; private set; }
+        public int Y { get; private set; }
 
-        public Airport()
-        {
-
-        }
+        public Airport(){}
         public Airport(string name)
         {
             this.Name = name;
+        }
+        public Airport(string name, int x, int y)
+        {
+            this.Name = name;
+            this.X = x;
+            this.Y = y;
         }
     }
 }
