9265cd5e8f318a983a4498999d9f7aa7cb00933e
Gabriel Audette <gabaudette@gmail.com>
2019-12-04 16:19:35 -0500

    Update PositionGUI.cs

--- TP-Builder/WindowsFormsApp1/PositionGUI.cs
+++ TP-Builder/WindowsFormsApp1/PositionGUI.cs
@@ -14,21 +14,7 @@ namespace TPBuilder
         private void PbMap_Click(object sender, EventArgs e)
         {
             MouseEventArgs me = (MouseEventArgs)e;
-
-            double x = (double)(me.X * 180) / pbMap.Width - 90;
-            double y = (double)(me.Y * 180) / (pbMap.Height) - 90;
-
-            double degX = Math.Floor(x);
-            double degY = Math.Floor(y);
-
-            double tempX = x - degX;
-            double tempY = y - degY;
-
-            double minX = Math.Abs(tempX * 60);
-            double minY = Math.Abs(tempY * 60);
-          
-            Console.WriteLine($"'minX: {minX}, minY:{minY} X: {x}, Y: {y}");
-            
+            GeoPosition.ConvertToDMS(me.X, me.Y, pbMap.Width, pbMap.Height);
         }
 
         private void PositionGUI_Load(object sender, EventArgs e)
