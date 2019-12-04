8b9ca98b29543b056c9703f04b92f8c28c38f1b2
Gabriel Audette <gabaudette@gmail.com>
2019-12-04 15:59:00 -0500

    Update ScenarioController.cs

--- TP-Builder/WindowsFormsApp1/ScenarioController.cs
+++ TP-Builder/WindowsFormsApp1/ScenarioController.cs
@@ -7,13 +7,52 @@ namespace TPBuilder
 {
     static class ScenarioController
     {
+        
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new BuilderGUI());
+        }
 
+        public static void CreateAirport(string name, int x, int y, int minPassenger, int maxPassenger, int minMarchandise, int maxMarchandise)
+        {
+            Scenario scenario = Scenario.Instance;
+            if (scenario.ValidAirport(name))
+                scenario.CreateAirport(name, x, y, minPassenger, maxPassenger, minMarchandise, maxMarchandise);
+            else
+                Console.WriteLine($"Error : Airport : {name} already exist!");
+        }
+
+        public static void AddCargoPlane(int airportID, string name, int speed, int maintenance, int loadingTime, int unLoadingTime, int weight)
+        {
+            Scenario scenario = Scenario.Instance;
+            scenario.AddCargoPlane(airportID, name, speed, maintenance, loadingTime, unLoadingTime, weight);
+        }
+
+        public static void AddPassengerPlane(int airportID, string name, int speed, int maintenance, int loadingTime, int unLoadingTime, int capacity)
+        {
+            Scenario scenario = Scenario.Instance;
+            scenario.AddPassengerPlane(airportID, name, speed, maintenance, loadingTime, unLoadingTime, capacity);
+        }
+
+        public static void AddWaterBomber(int airportID, string name, int speed, int maintenance, int droppingTime, int waterCapacity)
+        {
+            Scenario scenario = Scenario.Instance;
+            scenario.AddWaterBomber(airportID, name, speed, maintenance, droppingTime, waterCapacity);
+        }
+
+        public static void AddObserverPlane(int airportID, string name, int speed, int maintenance)
+        {
+            Scenario scenario = Scenario.Instance;
+            scenario.AddObserverPlane(airportID, name, speed, maintenance);
+        }
+
+        public static void AddRescueHelicopter(int airportID, string name, int speed, int maintenance)
+        {
+            Scenario scenario = Scenario.Instance;
+            scenario.AddRescueHelicopter(airportID, name, speed, maintenance);
         }
 
         static public void Serialize(string filename)
@@ -23,4 +62,5 @@ namespace TPBuilder
                 xs.Serialize(wr, Scenario.Instance);
         }
     }
+
 }
