e4ef91f3a7ed22b42f9ad14bf468f1fd5fed3b82
Gabriel Audette <gabaudette@gmail.com>
2019-12-06 13:12:44 -0500

    Update ScenarioController.cs

--- TP-Builder/WindowsFormsApp1/ScenarioController.cs
+++ TP-Builder/WindowsFormsApp1/ScenarioController.cs
@@ -7,7 +7,7 @@ namespace TPBuilder
 {
     static class ScenarioController
     {
-        static BuilderGUI GUI;
+        static BuilderGUI GUI; //The builder's view (GUI)
         [STAThread]
         static void Main()
         {
@@ -19,6 +19,16 @@ namespace TPBuilder
             Application.Run(GUI);
         }
 
+        /// <summary>
+        /// Resquest the scenario to create an airport
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="minPassenger"></param>
+        /// <param name="maxPassenger"></param>
+        /// <param name="minMarchandise"></param>
+        /// <param name="maxMarchandise"></param>
         public static void CreateAirport(string name, int x, int y, int minPassenger, int maxPassenger, int minMarchandise, int maxMarchandise)
         {
             Scenario scenario = Scenario.Instance;
@@ -27,37 +37,78 @@ namespace TPBuilder
             else
                 Console.WriteLine($"Error : Airport : {name} already exist!");
         }
-
+        /// <summary>
+        /// Resquest the aircraft factory to create a cargo plane
+        /// </summary>
+        /// <param name="airportID"></param>
+        /// <param name="name"></param>
+        /// <param name="speed"></param>
+        /// <param name="maintenance"></param>
+        /// <param name="loadingTime"></param>
+        /// <param name="unLoadingTime"></param>
+        /// <param name="weight"></param>
         public static void AddCargoPlane(int airportID, string name, int speed, int maintenance, int loadingTime, int unLoadingTime, int weight)
         {
             Scenario scenario = Scenario.Instance;
             scenario.AddCargoPlane(airportID, name, speed, maintenance, loadingTime, unLoadingTime, weight);
         }
-
+        /// <summary>
+        /// Resquest the aircraft factory to create a passenger plane
+        /// </summary>
+        /// <param name="airportID"></param>
+        /// <param name="name"></param>
+        /// <param name="speed"></param>
+        /// <param name="maintenance"></param>
+        /// <param name="loadingTime"></param>
+        /// <param name="unLoadingTime"></param>
+        /// <param name="capacity"></param>
         public static void AddPassengerPlane(int airportID, string name, int speed, int maintenance, int loadingTime, int unLoadingTime, int capacity)
         {
             Scenario scenario = Scenario.Instance;
             scenario.AddPassengerPlane(airportID, name, speed, maintenance, loadingTime, unLoadingTime, capacity);
         }
-
+        /// <summary>
+        /// Resquest the aircraft factory to create a water bomber
+        /// </summary>
+        /// <param name="airportID"></param>
+        /// <param name="name"></param>
+        /// <param name="speed"></param>
+        /// <param name="maintenance"></param>
+        /// <param name="droppingTime"></param>
+        /// <param name="waterCapacity"></param>
         public static void AddWaterBomber(int airportID, string name, int speed, int maintenance, int droppingTime, int waterCapacity)
         {
             Scenario scenario = Scenario.Instance;
             scenario.AddWaterBomber(airportID, name, speed, maintenance, droppingTime, waterCapacity);
         }
-
+        /// <summary>
+        /// Resquest the aircraft factory to create an observer plane
+        /// </summary>
+        /// <param name="airportID"></param>
+        /// <param name="name"></param>
+        /// <param name="speed"></param>
+        /// <param name="maintenance"></param>
         public static void AddObserverPlane(int airportID, string name, int speed, int maintenance)
         {
             Scenario scenario = Scenario.Instance;
             scenario.AddObserverPlane(airportID, name, speed, maintenance);
         }
-
+        /// <summary>
+        /// Resquest the aircraft factory to create a rescue helicopter
+        /// </summary>
+        /// <param name="airportID"></param>
+        /// <param name="name"></param>
+        /// <param name="speed"></param>
+        /// <param name="maintenance"></param>
         public static void AddRescueHelicopter(int airportID, string name, int speed, int maintenance)
         {
             Scenario scenario = Scenario.Instance;
             scenario.AddRescueHelicopter(airportID, name, speed, maintenance);
         }
-
+        /// <summary>
+        /// Request the scenario to serialize itself
+        /// </summary>
+        /// <param name="filename"></param>
         public static void Serialize(string filename)
         {
             XmlSerializer xs = new XmlSerializer(typeof(Scenario));
