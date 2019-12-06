3b5996eefd29e94896a18bde7437761d0b1266b8
Gabriel Audette <gabaudette@gmail.com>
2019-12-06 13:41:28 -0500

    Update PositionGUI.cs

--- TP-Builder/WindowsFormsApp1/PositionGUI.cs
+++ TP-Builder/WindowsFormsApp1/PositionGUI.cs
@@ -5,21 +5,27 @@ namespace TPBuilder
 {
     public partial class PositionGUI : Form
     {
+        public MouseEventArgs Me { get; private set; }
         public PositionGUI()
         {
             InitializeComponent();
             pbMap.Image = Properties.Resources.map1;
         }
 
+        private void PositionGUI_Load(object sender, EventArgs e) { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void PbMap_Click(object sender, EventArgs e)
         {
-            MouseEventArgs me = (MouseEventArgs)e;
-            GeoPosition.ConvertToDMS(me.X, me.Y, pbMap.Width, pbMap.Height);
-        }
+             Me = (MouseEventArgs)e;
+            double X = (double)(Me.X * 180) / pbMap.Width - 90;
+            double Y = (double)(Me.X * 180) / pbMap.Height - 90;
 
-        private void PositionGUI_Load(object sender, EventArgs e)
-        {
-
+            GeoPosition.ConvertToDMS(X, Y);
         }
     }
 }
