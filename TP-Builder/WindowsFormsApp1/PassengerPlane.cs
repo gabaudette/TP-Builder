d2f790f8423665b50c8723da45e45694085980b5
Gabriel Audette <gabaudette@gmail.com>
2019-12-08 13:15:08 -0500

    Update PassengerPlane.cs

--- TP-Builder/WindowsFormsApp1/PassengerPlane.cs
+++ TP-Builder/WindowsFormsApp1/PassengerPlane.cs
@@ -2,12 +2,11 @@
 {
     public class PassengerPlane : PassengerAircraft
     {
-        public int Capacity { get; set; }
-
+   
         public PassengerPlane() { }
-        public PassengerPlane(string name, int speed, int maintenance, int loadingTime, int unloadingTime , int capacity) : base(name, speed, maintenance, loadingTime, unloadingTime)
+        public PassengerPlane(string name, int speed, int maintenance, int loadingTime, int unloadingTime , int capacity) : base(name, speed, maintenance, loadingTime, unloadingTime, capacity)
         {
-            Capacity = capacity;
+         
         }
     }
 }
