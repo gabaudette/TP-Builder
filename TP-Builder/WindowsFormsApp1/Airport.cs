9ae9b88af9566e42de6c09da02aa77e2c617cdd4
Gabriel Audette <gabaudette@gmail.com>
2019-12-10 16:21:21 -0500

    Update Airport.cs

--- TP-Builder/WindowsFormsApp1/Airport.cs
+++ TP-Builder/WindowsFormsApp1/Airport.cs
@@ -5,28 +5,76 @@ namespace TPBuilder
 {
     public class Airport
     {
-        public string Name { get; set; } //Airport's name
-        public int X { get; set; } //Airport's X position
-        public int Y { get; set; } //Airport's Y position
-        public int MinPassenger { get; set; } //Airport's minimum passenger capacity
-        public int MaxPassenger { get; set; } //Airport's maximum passenger capacity
-        public int MinMarchandise { get; set; } //Airport's minimum marchandise capacity
-        public int MaxMarchandise { get; set; } //Airport's maximum marchandise capacity
+        private string name; //Airport's name
+        private int x; //Airport's X position
+        private int y; //Airport's Y position
+        private int minPassenger; //Airport's minimum passenger capacity
+        private int maxPassenger; //Airport's maximum passenger capacity
+        private int minMarchandise; //Airport's minimum marchandise capacity
+        private int maxMarchandise; //Airport's maximum marchandise capacity
+        private string dms; // Airport's position with by dms notation
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int X
+        {
+            get { return this.x; }
+            set { this.x = value; }
+        }
+
+        public int Y
+        {
+            get { return this.y; }
+            set { this.y = value; }
+        }
+
+        public int MinPassenger
+        {
+            get { return this.minPassenger; }
+            set { this.minPassenger = value; }
+        }
+
+        public int MaxPassenger
+        {
+            get { return this.maxPassenger; }
+            set { this.maxPassenger = value; }
+        }
+
+        public int MinMarchandise
+        {
+            get { return this.minMarchandise; }
+            set { this.minMarchandise = value; }
+        }
+        public int MaxMarchandise
+        {
+            get { return this.maxMarchandise; }
+            set { this.maxMarchandise = value; }
+        }
+
         [XmlIgnore]
-        public string DMS { get; set; }
+        public string DMS
+        {
+            get { return this.dms; }
+            set { this.dms = value; }
+        }
+
         public List<Aircraft> Aircrafts { get; set; } //List of all the aircraft inside the current airport
 
         public Airport() { }
         public Airport(string name, int x, int y, int minPassenger, int maxPassenger, int minMarchandise, int maxMarchandise, string dms)
         {
-            Name = name;
-            X = x;
-            Y = y;
-            MinPassenger = minPassenger;
-            MaxPassenger = maxPassenger;
-            MinMarchandise = minMarchandise;
-            MaxMarchandise = maxMarchandise;
-            DMS = dms;
+            this.name = name;
+            this.x = x;
+            this.y = y;
+            this.minPassenger = minPassenger;
+            this.maxPassenger = maxPassenger;
+            this.minMarchandise = minMarchandise;
+            this.maxMarchandise = maxMarchandise;
+            this.dms = dms;
             Aircrafts = new List<Aircraft>();
         }
 
