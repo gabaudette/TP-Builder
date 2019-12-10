0faf7e1f3d6a014af6900a0e31d26951b31699b3
Gabriel Audette <gabaudette@gmail.com>
2019-12-10 12:11:07 -0500

    Update GeoPosition.cs

--- TP-Builder/WindowsFormsApp1/GeoPosition.cs
+++ TP-Builder/WindowsFormsApp1/GeoPosition.cs
@@ -10,22 +10,7 @@ namespace TPBuilder
         /// <param name="x"></param>
         /// <param name="y"></param>
         /// <returns></returns>
-        public static string ConvertToDMS(double x, double y)
-        {
-            double degX = Math.Floor(x);
-            double degY = Math.Floor(y);
-
-            double tempDegX = x - degX;
-            double tempDegY = y - degY;
-
-            double minX = Math.Abs(tempDegX * 60);
-            double minY = Math.Abs(tempDegY * 60);
-
-            Console.WriteLine($"'minX: {minX}, minY:{minY} X: {x}, Y: {y}");
-            return null;
-        }
-
-        public static string getCoord(int x, int y, int width, int height)
+        public static string ConvertToDMS(int x, int y, int width, int height)
         {
             double degreFromMidX = Math.Abs(x - width / 2);
             double horizontalDegree = (degreFromMidX * 360) / width;
@@ -35,16 +20,14 @@ namespace TPBuilder
             double verticalDegree = (degreFromMidY * 360) / width;
             double verticalMin = (verticalDegree - Math.Floor(verticalDegree)) * 60;
 
-
-
             if ((x < width / 2) && (y < height / 2))
-                return $"{Math.Floor(verticalDegree)}° {Math.Floor(verticalMin)} S, {Math.Floor(horizontalDegree)}° {Math.Floor(horizontalMin)} O";
+                return $"{Math.Floor(verticalDegree)}° {Math.Floor(verticalMin)} ; S {Math.Floor(horizontalDegree)}° {Math.Floor(horizontalMin)} O";
             else if ((x > width / 2) && (y < height / 2))
-                return $"{Math.Floor(verticalDegree)}° {Math.Floor(verticalMin)} N, {Math.Floor(horizontalDegree)}° {Math.Floor(horizontalMin)} E";
+                return $"{Math.Floor(verticalDegree)}° {Math.Floor(verticalMin)} ;  N {Math.Floor(horizontalDegree)}° {Math.Floor(horizontalMin)} E";
             else if ((x > width / 2) && (y > height / 2))
-                return $"{Math.Floor(verticalDegree)}° {Math.Floor(verticalMin)} N, {Math.Floor(horizontalDegree)}° {Math.Floor(horizontalMin)} O";
+                return $"{Math.Floor(verticalDegree)}° {Math.Floor(verticalMin)} ;  N {Math.Floor(horizontalDegree)}° {Math.Floor(horizontalMin)} O";
             else
-                return $"{Math.Floor(verticalDegree)}° {Math.Floor(verticalMin)} S, {Math.Floor(horizontalDegree)}° {Math.Floor(horizontalMin)} E";
+                return $"{Math.Floor(verticalDegree)}° {Math.Floor(verticalMin)} ; S {Math.Floor(horizontalDegree)}° {Math.Floor(horizontalMin)} E";
         }
     }
 }
