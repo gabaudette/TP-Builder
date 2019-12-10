4884e693913f6f4551380385febc2b1f971fc489
Gabriel Audette <gabaudette@gmail.com>
2019-12-10 16:10:19 -0500

    Update PositionGUI.Designer.cs

--- TP-Builder/WindowsFormsApp1/PositionGUI.Designer.cs
+++ TP-Builder/WindowsFormsApp1/PositionGUI.Designer.cs
@@ -38,17 +38,18 @@
             this.pbMap.Location = new System.Drawing.Point(27, 17);
             this.pbMap.Margin = new System.Windows.Forms.Padding(2);
             this.pbMap.Name = "pbMap";
-            this.pbMap.Size = new System.Drawing.Size(1538, 908);
+            this.pbMap.Size = new System.Drawing.Size(1026, 591);
             this.pbMap.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pbMap.TabIndex = 0;
             this.pbMap.TabStop = false;
             this.pbMap.Click += new System.EventHandler(this.PbMap_Click);
+            this.pbMap.Paint += new System.Windows.Forms.PaintEventHandler(this.PbMap_Paint);
             // 
             // PositionGUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1576, 1041);
+            this.ClientSize = new System.Drawing.Size(1084, 631);
             this.Controls.Add(this.pbMap);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
