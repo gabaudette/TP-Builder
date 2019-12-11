1dd619566dc0b3291d32568aac79abd595335dec
Gabriel Audette <gabaudette@gmail.com>
2019-12-10 19:01:01 -0500

    Update PassengerAircraft.cs

--- TP-Builder/WindowsFormsApp1/PassengerAircraft.cs
+++ TP-Builder/WindowsFormsApp1/PassengerAircraft.cs
@@ -2,17 +2,35 @@
 {
     public class PassengerAircraft : Aircraft
     {
-        public int LoadingTime { get; set; }
-        public int UnloadingTime { get; set; }
-        public int MaxCapacity { get; set; }
+        protected int loadingTime; //Time it take to load client
+        protected int unloadingTime; //Time it take to unload client
+        protected int maxCapacity; // Maximum capacity
+
+        public int LoadingTime
+        {
+            get { return this.loadingTime; }
+            set { this.loadingTime = value; }
+        }
+
+        public int UnloadingTime
+        {
+            get { return this.unloadingTime; }
+            set { this.unloadingTime = value; }
+        }
+
+        public int MaxCapacity
+        {
+            get { return this.maxCapacity; }
+            set { this.maxCapacity = value; }
+        }
 
         public PassengerAircraft() { }
 
         public PassengerAircraft(string name, int speed, int maintenance, int loadingTime, int unloadingTime, int capacity) : base(name, speed , maintenance)
         {
-            LoadingTime = loadingTime;
-            UnloadingTime = unloadingTime;
-            MaxCapacity = capacity;
+            this.loadingTime = loadingTime;
+            this.unloadingTime = unloadingTime;
+            this.maxCapacity = capacity;
         }
     }
 }
