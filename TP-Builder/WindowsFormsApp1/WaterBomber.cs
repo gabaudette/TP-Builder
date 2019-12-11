32f0534bb419e7991c2278620d646005981e9a4d
Gabriel Audette <gabaudette@gmail.com>
2019-12-10 19:01:03 -0500

    Update WaterBomber.cs

--- TP-Builder/WindowsFormsApp1/WaterBomber.cs
+++ TP-Builder/WindowsFormsApp1/WaterBomber.cs
@@ -2,15 +2,27 @@
 {
     public class WaterBomber : Aircraft
     {
-        public int WaterCapacity { get; set; }
-        public int DroppingTime { get; set; }
-        
+        private int droppingTime; // Time it take to drop its load
+        private int waterCapacity; // Water capacity of the water bomber
+
+        public int DroppingTime
+        {
+            get { return this.droppingTime; }
+            set { this.droppingTime = value; }
+        }
+
+        public int WaterCapacity
+        {
+            get { return this.waterCapacity; }
+            set { this.waterCapacity = value; }
+        }
+
         public WaterBomber() { }
 
         public WaterBomber(string name, int speed, int maintenance, int waterCapacity, int droppingTime) : base(name, speed, maintenance)
         {
-            WaterCapacity = waterCapacity;
-            DroppingTime = droppingTime;
+            this.waterCapacity = waterCapacity;
+            this.droppingTime = droppingTime;
         }
     }
 }
